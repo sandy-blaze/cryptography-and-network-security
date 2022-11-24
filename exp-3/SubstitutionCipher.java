ed1b359f234b4e4b6e07b6a1d39f825f7b4bed1a
Sandeep Kumar Bhaviri <86642174+sandy-blaze@users.noreply.github.com>
2022-11-24 19:56:10 +0530

    Update SubstitutionCipher.java

--- exp-3/SubstitutionCipher.java
+++ exp-3/SubstitutionCipher.java
@@ -1,21 +1,28 @@
 import java.util.*;
 
 class SubstitutionCipher {
+    public static int[] rKey = new int[256];
+    public static ArrayList<Integer> key = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 256; ++i) {
+            key.add(i);
+        }
+        Collections.shuffle(key);
+        for (int i = 0; i < 256; ++i) {
+            rKey[key.get(i)] = i;
+        }
         String plainText = scanner.nextLine();
-        String cipherText = encypt(plainText); 
+        String cipherText = encypt(plainText);
         System.out.println(cipherText);
         System.out.println(decypt(cipherText));
         scanner.close();
     }
     public static String encypt(String plainText) {
-        int shift = 3;
         char[] plainTextArray = plainText.toCharArray();
         StringBuilder cipherTextArray = new StringBuilder(plainTextArray.length);
         for (char character : plainTextArray) {
-            if (character == ' ') cipherTextArray.append(' ');
-            else cipherTextArray.append((char)((((int)character - 97 + shift) % 26 + 26) % 26 + 97));
+            cipherTextArray.append((char)(int)key.get(character));
         }
         return cipherTextArray.toString();
     }
@@ -24,8 +31,7 @@ class SubstitutionCipher {
         char[] plainTextArray = plainText.toCharArray();
         StringBuilder cipherTextArray = new StringBuilder(plainTextArray.length);
         for (char character : plainTextArray) {
-            if (character == ' ') cipherTextArray.append(' ');
-            else cipherTextArray.append((char)((((int)character - 97 - shift) % 26 + 26) % 26 + 97));
+            cipherTextArray.append((char)rKey[character]);
         }
         return cipherTextArray.toString();
     }
